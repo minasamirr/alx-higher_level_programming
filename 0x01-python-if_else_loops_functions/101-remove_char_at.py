39e718580b79fbe2bc33a239431c8b9835810909
minasamirr <1701538@eng.asu.edu.eg>
2023-11-28 09:36:19 -0800

    task

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
+    if n < 0 or n >= len(str):
+        return str
+
     removed_char = str[n]
     new_str = ""
     for i in range(len(str)):
         if i != n:
             new_str += str[i]
-    return new_str
+    return new_str, removed_char
+
