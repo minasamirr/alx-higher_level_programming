d42586bad0bc81933e8bf97b8c7ba592a7c440fa
minasamirr <1701538@eng.asu.edu.eg>
2023-11-28 08:42:25 -0800

    task

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(99):
+for i in range(100):
     if i < 99:
         print("{:02d}".format(i), end=', ')
     else:
-        print("{:02d}".format(i + 1))
+        print("{:02d}".format(i))
