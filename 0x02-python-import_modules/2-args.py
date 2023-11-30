ade4aaa4f0fa8ff335f5c21e332fa399eea5cff0
minasamirr <1701538@eng.asu.edu.eg>
2023-11-30 10:59:00 -0800

    task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 def print_arg(argv):
-    count = len(argv) - 1
-    if count == 0:
-        print("{:d} argument.".format(count))
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d} argument.".format(n))
         return
     else:
-        if count == 1:
-            print("{:d} argument:".format(count))
+        if n == 1:
+            print("{:d} argument:".format(n))
         else:
-            print("{:d} arguments:".format(count))
+            print("{:d} arguments:".format(n))
         i = 1
-        while i <= count:
+        while i <= n:
             print("{:d}: {:s}".format(i, argv[i]))
             i += 1
 
