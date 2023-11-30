6d779ed9474fd67c2d4b9a4ae15748be4249e1e7
minasamirr <1701538@eng.asu.edu.eg>
2023-11-30 10:39:58 -0800

    task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 import sys
 
-# Get the number of arguments
-num_args = len(sys.argv) - 1  # Subtract 1 for the script name
+num_args = len(sys.argv) - 1
 
-# Print the number of arguments
 if num_args == 0:
     print("0 arguments.")
 else:
