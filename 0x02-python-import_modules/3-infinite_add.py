fed9ab5de46c1963e23ec1402362bdbdf8571beb
minasamirr <1701538@eng.asu.edu.eg>
2023-11-30 11:02:21 -0800

    task

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
-add = 0
-for s in argv[1:]:
-    add += int(s)
-print("{:d}".format(add))
+import sys
+
+# Function to add all arguments
+def infinite_add(*args):
+    return sum(map(int, args))
+
+# Get the result of the addition
+result = infinite_add(*sys.argv[1:])
+
+# Print the result followed by a new line
+print(result)
+
