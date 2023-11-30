83dc3c213fd4325df864707b7ad0c74c7a7f3990
minasamirr <1701538@eng.asu.edu.eg>
2023-11-30 11:00:12 -0800

    task

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+import hidden_4
+
+
+def discovr():
+    name = dir(hidden_4)
+    for i in name:
+        if i[:2] != '__':
+            print("{:s}".format(i))
+
+
 if __name__ == "__main__":
-    import hidden_4
-    for s in dir(hidden_4):
-        if s[:2] != "__":
-            print("{:s}".format(s))
+    discovr()
