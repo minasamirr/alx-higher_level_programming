6ae8369c99e756009030f52e507bb3778beb1c1f
minasamirr <1701538@eng.asu.edu.eg>
2023-11-30 10:47:43 -0800

    task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 import sys
 
 num_args = len(sys.argv) - 1
-
 if num_args == 0:
     print("0 arguments.")
+elif num_args == 1:
+    print("1 argument:")
 else:
-    print("{} argument{}:".format(num_args, 's' if num_args > 1 else ''))
-
-    for i in range(num_args):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    print("{} arguments:".format(num_args))
+for i in range(num_args):
+     print("{}: {}".format(i + 1, sys.argv[i + 1]))
