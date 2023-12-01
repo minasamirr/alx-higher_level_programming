3b95a5130a003862c644688bb756312b18769c64
minasamirr <1701538@eng.asu.edu.eg>
2023-12-01 01:29:30 -0800

    task

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-import sys
 def infinite_add(*args):
     return sum(map(int, args))
 result = infinite_add(*sys.argv[1:])
 print(result)
+
+
+if __name__ == "__main__":
+    import sys
+    result = infinite_add(*sys.argv[1:])
+    print(result)
