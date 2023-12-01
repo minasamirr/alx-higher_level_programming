34f42d137b6953d5aceaa192c239c115f9e5a44b
minasamirr <1701538@eng.asu.edu.eg>
2023-12-01 01:59:09 -0800

    task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
+
+
 def argv_calculator(argv):
     if len(argv) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
