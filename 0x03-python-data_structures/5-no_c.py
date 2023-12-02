8d38d4f0f25f8a813f3cc24827c6fcf3ffa22464
minasamirr <1701538@eng.asu.edu.eg>
2023-12-02 03:14:22 -0800

    task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,7 @@
 def no_c(my_string):
     return ''.join(char for char in my_string if char.lower() not in {'c'})
 
+
 if __name__ == "__main__":
     print(no_c("Best School"))
     print(no_c("Chicago"))
