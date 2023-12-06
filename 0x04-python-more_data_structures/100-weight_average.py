8c84744f20f9f2bb52b183ae4e49b2375cac1fdd
minasamirr <1701538@eng.asu.edu.eg>
2023-12-06 08:34:47 -0800

    task

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 def weight_average(my_list=[]):
     weighted_sum, total_weight = map(sum, zip(*[(score * weight, weight) for score, weight in my_list]))
     if total_weight == 0:
