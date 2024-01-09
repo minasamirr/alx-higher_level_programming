1fd29ac3a7fa16cabecb385752db03e33ddd7a1e
minasamirr <1701538@eng.asu.edu.eg>
2024-01-09 10:40:14 -0800

    task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,8 +3,7 @@
 Script for adding arguments to a Python list and saving them to a file.
 """
 
-import sys
-from os import path
+from sys import argv
 from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
 
