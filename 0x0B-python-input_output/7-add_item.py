5c4e7617b8d8f5f278b3005dfa04436747428987
minasamirr <1701538@eng.asu.edu.eg>
2024-01-09 10:39:15 -0800

    task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,36 +8,14 @@ from os import path
 from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
 
+filename = "add_item.json"
 
-def add_item(args):
-    """
-    Function to add arguments to a Python list and save them to a file.
+try:
+    json_list = load_from_json_file(filename)
+except FileNotFoundError:
+    json_list = []
 
-    Parameters:
-    - args (list): The list of arguments.
+for arg in argv[1:]:
+    json_list.append(arg)
 
-    Returns:
-    - None
-    """
-    filename = "add_item.json"
-
-    # Check if the file exists, if not, create it
-    if not path.exists(filename):
-        save_to_json_file([], filename)
-
-    # Load the existing list from the file
-    my_list = load_from_json_file(filename)
-
-    # Add the new arguments to the list
-    my_list.extend(args)
-
-    # Save the updated list to the file
-    save_to_json_file(my_list, filename)
-
-
-if __name__ == "__main__":
-    # Extract command line arguments excluding the script name
-    arguments = sys.argv[1:]
-
-    # Add the arguments to the list and save to file
-    add_item(arguments)
+save_to_json_file(json_list, filename)
