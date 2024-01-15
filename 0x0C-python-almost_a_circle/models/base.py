9245e587730f43dd627a6e148239012db74f34fe
minasamirr <mina430q@gmail.com>
2024-01-15 11:03:49 -0800

    task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,23 +1,45 @@
 #!/usr/bin/python3
 """Module for the Base class"""
+import json
 
 
 class Base:
-    """The base class for managing id attribute"""
+    """The Base class"""
 
-    # Private class attribute
     __nb_objects = 0
 
     def __init__(self, id=None):
         """Class constructor
 
         Args:
-            id (int): An integer representing the id (default is None)
+            id (int): Identifier for the object (default is None)
         """
         if id is not None:
-            # Assign the public instance attribute id with the given value
             self.id = id
         else:
-            # Increment __nb_objects and assign the new value to id
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Convert list of dictionaries to JSON string representation
+
+        Args:
+            list_dictionaries (list): List of dictionaries
+
+        Returns:
+            str: JSON string representation of list_dictionaries
+        """
+        if not list_dictionaries or list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+
+if __name__ == "__main__":
+    r1 = Rectangle(10, 7, 2, 8)
+    dictionary = r1.to_dictionary()
+    json_dictionary = Base.to_json_string([dictionary])
+    print(dictionary)
+    print(type(dictionary))
+    print(json_dictionary)
+    print(type(json_dictionary))
