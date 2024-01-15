29fa3569e7fb51a9af46f99751415ba1df87a56f
minasamirr <mina430q@gmail.com>
2024-01-15 10:57:12 -0800

    task

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,8 +15,8 @@ class Square(Rectangle):
             y (int): y-coordinate of the square (default is 0)
             id (int): Identifier for the square (default is None)
         """
-        # Call the super class with id, x, y, width, and height
         super().__init__(size, size, x, y, id)
+        # Call the super class with id, x, y, width, and height
         self.size = size
 
     @property
@@ -40,19 +40,11 @@ class Square(Rectangle):
 if __name__ == "__main__":
     s1 = Square(5)
     print(s1)
-    print(s1.area())
-    s1.display()
+    print(s1.size)
+    s1.size = 10
+    print(s1)
 
-    print("---")
-
-    s2 = Square(2, 2)
-    print(s2)
-    print(s2.area())
-    s2.display()
-
-    print("---")
-
-    s3 = Square(3, 1, 3)
-    print(s3)
-    print(s3.area())
-    s3.display()
+    try:
+        s1.size = "9"
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
