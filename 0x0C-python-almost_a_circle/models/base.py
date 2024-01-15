9523d3e26ff604aba69678a6076859d0f194e54e
minasamirr <mina430q@gmail.com>
2024-01-15 11:15:34 -0800

    task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module for the Base class"""
+
 import json
 
 
@@ -23,6 +24,16 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Return JSON string representation of list_dictionaries"""
-        if list_dictionaries is None or list_dictionaries == []:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+
+if __name__ == "__main__":
+    r1 = Rectangle(10, 7, 2, 8)
+    dictionary = r1.to_dictionary()
+    json_dictionary = Base.to_json_string([dictionary])
+    print(dictionary)
+    print(type(dictionary))
+    print(json_dictionary)
+    print(type(json_dictionary))
