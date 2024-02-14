537689235b0f42d408e04e3e509db1fd8077c0d0
minasamirr <1701538@eng.asu.edu.eg>
2024-02-14 09:57:02 -0800

    task

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,6 +2,6 @@
 -- from the hbtn_0d_tvshows database. Results are sorted in ascending order by the show title and genre name.
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
