6bab3b6d9bc271d48e089dd2b342dff0c29f797d
minasamirr <1701538@eng.asu.edu.eg>
2024-02-14 09:56:12 -0800

    task

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,8 @@
--- Lists all genres of the show Dexter from the hbtn_0d_tvshows database, sorted in ascending order by genre name.
+-- Lists all genres of the show Dexter in the database hbtn_0d_tvshows.
+-- Records are ordered by ascending genre name.
 SELECT tv_genres.name
-FROM tv_show_genres
-JOIN tv_shows ON tv_show_genres.tv_show_id = tv_shows.id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_shows.title = 'Dexter'
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = "Dexter"
 ORDER BY tv_genres.name ASC;
--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,10 @@
--- Lists all Comedy shows from the hbtn_0d_tvshows database, sorted in ascending order by the show title.
+-- Lists all comedy shows in the database hbtn_0d_tvshows.
+-- Records are ordered by descending show title.
 SELECT tv_shows.title
-FROM tv_show_genres
-JOIN tv_shows ON tv_show_genres.tv_show_id = tv_shows.id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_shows.title ASC;
+FROM tv_shows
+    INNER JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
+    INNER JOIN tv_genres
+    ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
