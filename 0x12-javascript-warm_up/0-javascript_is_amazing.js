b7dcf7c5b6760469417a276ac9bb1539afb41f47
minasamirr <mina430q@gmail.com>
2024-03-12 17:47:32 +0200

    task

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -3,7 +3,7 @@
 // 0-javascript_is_amazing.js
 
 // Create a constant variable called myVar with the value “JavaScript is amazing”
-const myVar = "JavaScript is amazing";
+const myVar = 'JavaScript is amazing';
 
 // Use console.log(...) to print all output
 console.log(myVar);
