09a486545ee93d8b1edcfe01db9b5c138d88fcf2
minasamirr <mina430q@gmail.com>
2024-03-16 12:11:23 +0200

    .

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+# Connect to MySQL server
+# Create a cursor object using cursor() method
+# Execute SQL query to select all states sorted by id
+# Fetch all the rows using fetchall() method
+# Print the result
+# Disconnect from server
 import MySQLdb
 import sys
 
@@ -6,22 +12,10 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-
-    # Connect to MySQL server
     db = MySQLdb.connect(user=username, passwd=password, db=database)
-
-    # Create a cursor object using cursor() method
     cursor = db.cursor()
-
-    # Execute SQL query to select all states sorted by id
     cursor.execute("SELECT * FROM states ORDER BY id")
-
-    # Fetch all the rows using fetchall() method
     data = cursor.fetchall()
-
-    # Print the result
     for row in data:
         print(row)
-
-    # Disconnect from server
     db.close()
