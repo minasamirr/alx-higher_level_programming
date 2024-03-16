ce543e78ba8371c1afe2f9f1548c89a45f8e2801
minasamirr <mina430q@gmail.com>
2024-03-16 12:22:07 +0200

    .

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
-# Connect to MySQL server
-# Create a cursor object using cursor() method
-# Execute SQL query to select all states sorted by id
-# Fetch all the rows using fetchall() method
-# Print the result
-# Disconnect from server
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+    # Create a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # Execute SQL query to select all states sorted by id
+    cursor.execute("SELECT * FROM states ORDER BY id")
+
+    # Fetch all the rows using fetchall() method
+    data = cursor.fetchall()
+
+    # Print the result
+    for row in data:
+        print(row)
+
+    # Disconnect from server
+    db.close()
