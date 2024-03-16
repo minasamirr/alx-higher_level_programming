2235aa8783459ee53ec074b6c1ead51e54c9c4e5
minasamirr <1701538@eng.asu.edu.eg>
2024-03-16 13:00:04 +0200

    Task

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
     database = sys.argv[3]
 
     # Create engine to connect to MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username, password, database))
 
     # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
