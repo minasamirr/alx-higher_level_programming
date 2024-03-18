ae5044edc4d76ceb4599bc55b94cde66f2de5778
minasamirr <1701538@eng.asu.edu.eg>
2024-03-18 05:09:37 -0700

    task

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,29 +1,71 @@
 #!/usr/bin/python3
+"""
+Script to display all values in the states table of hbtn_0e_0_usa where name
+matches the argument, safely.
+
+Usage: ./3-my_safe_filter_states.py username password database state_name
+
+Arguments:
+    username: MySQL username
+    password: MySQL password
+    database: Name of the database containing the states table
+    state_name: Name of the state to search for
+
+Requirements:
+    - MySQLdb module
+"""
+
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
+
+def main():
+    """
+    Main function to execute the script.
+    """
+    # Check if all required arguments are provided
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database state_name".format(
+            sys.argv[0]))
+        sys.exit(1)
+
+    # Get arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     state_name = sys.argv[4]
 
     # Connect to MySQL server
-    db = MySQLdb.connect(user=username, passwd=password, db=database)
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
 
-    # Create a cursor object using cursor() method
+    # Create a cursor object
     cursor = db.cursor()
 
-    # Execute SQL query to select states where name matches the provided argument safely
-    sql_query = "SELECT * FROM states WHERE name = %s ORDER BY id"
-    cursor.execute(sql_query, (state_name,))
+    # Execute the query using parameterized query to prevent SQL injection
+    try:
+        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC;"
+        cursor.execute(query, (state_name,))
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
 
-    # Fetch all the rows using fetchall() method
-    data = cursor.fetchall()
-
-    # Print the result
-    for row in data:
-        print(row)
-
-    # Disconnect from server
+    # Close cursor and database connection
+    cursor.close()
     db.close()
+
+
+if __name__ == "__main__":
+    main()
