9811fc35be7ae9c9cf350205f5b8a02dc69e69f7
minasamirr <mina430q@gmail.com>
2024-03-28 10:40:39 -0700

    task

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
+Script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
 """
 
 import requests
@@ -19,7 +20,8 @@ if __name__ == "__main__":
     try:
         json_response = response.json()
         if json_response:
-            print("[{}] {}".format(json_response.get('id'), json_response.get('name')))
+            print("[{}] {}".format(
+                json_response.get('id'), json_response.get('name')))
         else:
             print("No result")
     except ValueError:
