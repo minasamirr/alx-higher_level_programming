8febe5ab0fee0b59a6d489b90e3accd499c0e2f2
minasamirr <mina430q@gmail.com>
2024-03-28 19:35:39 +0200

    task

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    data = {'q': q}
+
+    response = requests.post("http://0.0.0.0:5000/search_user", data=data)
+
+    try:
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response.get('id'), json_response.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
