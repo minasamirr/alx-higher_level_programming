1baab3b16d006b55768aeb7b94251430e0401ee2
minasamirr <mina430q@gmail.com>
2024-04-23 19:28:19 +0200

    task

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,23 +1,6 @@
 #!/usr/bin/node
-
+// Write a script that writes a string to a file.
 const fs = require('fs');
-
-// Get the file path and string to write from command line arguments
-const filePath = process.argv[2];
-const stringToWrite = process.argv[3];
-
-// Check if file path and string to write are provided
-if (!filePath || !stringToWrite) {
-  console.error('Usage: ./1-writeme.js <file-path> "<string-to-write>"');
-  process.exit(1);
-}
-
-// Write the string to the file
-fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
-  if (err) {
-    // Print error object if an error occurred during writing
-    console.error(err);
-    return;
-  }
-  console.log('The file has been saved!');
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,22 +1,7 @@
 #!/usr/bin/node
-
+// Write a script that display the status code of a GET request.
 const request = require("request");
-
-// Get the URL to request from command line arguments
 const url = process.argv[2];
-
-// Check if URL is provided
-if (!url) {
-  console.error("Usage: ./2-statuscode.js <URL>");
-  process.exit(1);
-}
-
-// Send a GET request to the URL
-request.get(url, (error, response) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  // Print the status code
+request.get(url).on('response', function (response) {
   console.log(`code: ${response.statusCode}`);
 });
