190afd67d6522a4e264ea00840fd5b3a40ee66ab
minasamirr <mina430q@gmail.com>
2024-04-23 19:46:55 +0200

    task

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -16,39 +16,19 @@ const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 // Send a GET request to the Star Wars API endpoint
 request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
-
-  // Check if request was successful (status code 200)
-  if (response.statusCode !== 200) {
-    console.error(`Failed to retrieve data: Status code ${response.statusCode}`);
-    return;
-  }
-
-  // Parse the response body to JSON
-  const movieData = JSON.parse(body);
-
-  // Print the names of characters in the order they appear in the response
-  movieData.characters.forEach(characterUrl => {
-    request.get(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-
-      // Check if request was successful (status code 200)
-      if (response.statusCode !== 200) {
-        console.error(`Failed to retrieve data: Status code ${response.statusCode}`);
-        return;
-      }
-
-      // Parse the response body to JSON
-      const characterData = JSON.parse(body);
-
-      // Print the name of the character
-      console.log(characterData.name);
-    });
-  });
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
