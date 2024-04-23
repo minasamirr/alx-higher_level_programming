03b17d3cfff034b6cdefbdedf7eb46a5859fd3c3
minasamirr <1701538@eng.asu.edu.eg>
2024-04-23 19:11:39 +0200

    Task

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,23 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-// Get the file path from command line arguments
-const filePath = process.argv[2];
-
-// Check if file path is provided
-if (!filePath) {
-  console.error('Usage: ./0-readme.js <file-path>');
-  process.exit(1);
-}
-
-// Read the content of the file
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    // Print error object if an error occurred during reading
-    console.error(err);
-    return;
-  }
-  // Print the content of the file
-  console.log(data);
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+	  console.log(error || content);
 });
