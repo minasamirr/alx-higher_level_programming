36f6d3e590ab2d6e08ed8a84d58cb202052dc35a
minasamirr <mina430q@gmail.com>
2024-04-23 19:34:37 +0200

    task

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,37 +1,5 @@
 #!/usr/bin/node
-
+// Write a script that gets the contents of a webpage and stores it in a file.
 const fs = require('fs');
 const request = require('request');
-
-// Get the URL and file path from command line arguments
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-// Check if URL and file path are provided
-if (!url || !filePath) {
-  console.error('Usage: ./5-request_store.js <URL> <file-path>');
-  process.exit(1);
-}
-
-// Send a GET request to the URL
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  // Check if request was successful (status code 200)
-  if (response.statusCode !== 200) {
-    console.error(`Failed to retrieve data: Status code ${response.statusCode}`);
-    return;
-  }
-
-  // Write the response body to the file
-  fs.writeFile(filePath, body, 'utf8', (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(`The content has been saved to ${filePath}`);
-  });
-});
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
